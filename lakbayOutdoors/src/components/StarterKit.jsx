5cd7c541a0f579a7c308ff041d5d9c8690c155f7
Michaella Merza <michaellamerza@gmail.com>
2026-01-07 17:14:52 +0800

    fixed cart error

--- lakbayOutdoors/src/components/StarterKit.jsx
+++ lakbayOutdoors/src/components/StarterKit.jsx
@@ -15,21 +15,21 @@ const products = {
         {
             id: 1,
             name: "Hiking Boots",
-            price: "₱4,500",
+            price: 4500,
             desc: "Rugged waterproof boots designed for the muddy trails of Luzon's highest peaks.",
             img: "https://m.media-amazon.com/images/I/71p2KL9ZXTL._AC_SY395_.jpg"
         },
         {
             id: 5,
             name: "Carbon Fiber Trekking Poles",
-            price: "₱1,800",
+            price: 1800,
             desc: "Ultralight adjustable poles to save your knees on steep descents.",
             img: "https://m.media-amazon.com/images/I/6134PIDPt0L._AC_SX425_.jpg"
         },
         {
             id: 8,
             name: "40L Waterproof Backpack",
-            price: "₱2,800",
+            price: 2800,
             desc: "Spacious and durable daypack with built-in rain cover.",
             img: "https://m.media-amazon.com/images/I/81siwI2UxEL._AC_SY300_SX300_QL70_FMwebp_.jpg"
         }
@@ -38,14 +38,14 @@ const products = {
         {
             id: 2,
             name: "Apo Lightweight Tent",
-            price: "₱3,200",
+            price: 3200,
             desc: "Free-standing 2-person tent with excellent ventilation.",
             img: "https://m.media-amazon.com/images/I/71sbNSEx1iL._AC_SY300_SX300_QL70_FMwebp_.jpg"
         },
         {
             id: 15,
             name: "LED Headlamp (400 Lumens)",
-            price: "₱950",
+            price: 950,
             desc: "Hands-free lighting for night trekking and camp chores.",
             img: "https://m.media-amazon.com/images/I/61bIa8AnexL._AC_SY300_SX300_QL70_FMwebp_.jpg"
         }
@@ -54,14 +54,14 @@ const products = {
         {
             id: 3,
             name: "Pro Rock Climbing Harness",
-            price: "₱5,500",
+            price: 5500,
             desc: "Padded waist belt and leg loops for all-day comfort at the crag.",
             img: "https://m.media-amazon.com/images/I/71JceyaGVLL._AC_SY300_SX300_QL70_FMwebp_.jpg"
         },
         {
             id: 4,
             name: "Carabiner Set (Pack of 5)",
-            price: "₱1,200",
+            price: 5500,
             desc: "Heavy-duty screw-lock carabiners for anchoring and belaying.",
             img: "https://m.media-amazon.com/images/I/81iRzjms5AL._AC_SY300_SX300_QL70_FMwebp_.jpg"
         }
@@ -131,7 +131,7 @@ const products = {
                                 </div>
                                 <div className="kit-details">
                                     <h3>{item.name}</h3>
-                                    <span className="price">{item.price}</span>
+                                    <span className="price">₱{item.price}</span>
                                     <p>{item.desc}</p>
                                     <button className="add-kit-btn" onClick={() => {handleAddToKit(item);}}>
                                         ADD TO KIT
